b272ba4b9f41aaf1acdf46e96bff812d471d6b2c
michael di fiore <difioremph@gmail.com>
2023-02-24 12:23:23 -0500

    all done the validations

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,7 +2,5 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
   validates :comment, length:{minimum: 6 }
-  # validates :email, uniqueness: true
-  validates :list, uniqueness: true
-  validates :movie, uniqueness: true
+  validates :movie, uniqueness: {scope: :list}
 end
